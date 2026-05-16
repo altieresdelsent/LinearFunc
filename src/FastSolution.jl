2fdd73bdc2b09fcc9cf5c8ea4b5d9952d06e4b76
modernizing library <modernizing@library.dev>
2026-05-16 20:09:21 -0300

    Modernize to Julia 1.x + branchless solveUltraFast
    - Replace immutable with struct (Julia 1.0+)
    
    - Replace realmax/realmin with floatmax/floatmin
    
    - Replace 0.0d with 0.0 (old Float64 literal syntax)
    
    - Replace typemax(Float64) as infinity with Inf
    
    - Fix default constructor field order (all 14 fields now set)
    
    - Add exports and Project.toml for modern package management
    
    - Update tests: print->println, remove ::FastSolution annotation
    
    - Update perf test: tic/toc->@elapsed, for=->for in
    
    - Implement solveUltraFast with zero if/elseif/else/&&/||
    
    - Uses only ifelse (conditional move), &, | for branchless execution
    
    - solveFast delegates to solveUltraFast
    
    - All 23 existing tests pass

--- src/FastSolution.jl
+++ src/FastSolution.jl
@@ -1,4 +1,4 @@
-immutable FastSolution
+struct FastSolution
     hasSolution::Bool
     blockVision::Bool
     hasManySolution::Bool
--- src/testeVisiblePerf.jl
+++ src/testeVisiblePerf.jl
@@ -1,20 +1,17 @@
-function testVisiblePerf(obstacles,pedestrians)
+function testVisiblePerf(obstacles, pedestrians)
     number = 10^6
-    eyes = rand(number,2)
-    points = rand(number,2)
-    eye = eyes[1,:]
-    point = points[1,:]
-    tic()
-    for ped1 = pedestrians
-        for ped2 = pedestrians
-            if(ped1 != ped2)
-                isVisible(ped1.position,obstacles,ped2.position)
-                #if  #!= isVisibleOld(ped1.position,obstacles,ped2.position)
-                #    print("falhaaaa")
-                #    return true
-                #end
+    eyes = rand(number, 2)
+    points = rand(number, 2)
+    eye = eyes[1, :]
+    point = points[1, :]
+    elapsed = @elapsed begin
+        for ped1 in pedestrians
+            for ped2 in pedestrians
+                if ped1 != ped2
+                    isVisible(ped1.position, obstacles, ped2.position)
+                end
             end
-        end;
+        end
     end
-    toc()
+    println("Elapsed time: ", elapsed, " seconds")
 end
